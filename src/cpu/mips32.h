40894b912f09e6f5165e852485da356eede16b02
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-06-11 22:39:18 -0400

    Finish

--- src/cpu/mips32.h
+++ src/cpu/mips32.h
@@ -4,28 +4,27 @@
 
 #define MIPS32_INSTR(NAME) void NAME(r4300i_t* cpu, mips32_instruction_t instruction)
 
-void add(r4300i_t* cpu, mips32_instruction_t instruction);
-//void addi(r4300i_t* cpu, mips32_instruction_t instruction);
-MIPS32_INSTR(NAME);
-void addiu(r4300i_t* cpu, mips32_instruction_t instruction);
-void addu(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(add);
+MIPS32_INSTR(addi);
+MIPS32_INSTR(addiu);
+MIPS32_INSTR(addu);
 
-void and(r4300i_t* cpu, mips32_instruction_t instruction);
-void andi(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(and);
+MIPS32_INSTR(andi);
 
-void beq(r4300i_t* cpu, mips32_instruction_t instruction);
-void beql(r4300i_t* cpu, mips32_instruction_t instruction);
-void bne(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(beq);
+MIPS32_INSTR(beql);
+MIPS32_INSTR(bne);
 
-void jal(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(jal);
 
-void slti(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(slti);
 
-void mtc0(r4300i_t* cpu, mips32_instruction_t instruction);
-void lui(r4300i_t* cpu, mips32_instruction_t instruction);
-void lw(r4300i_t* cpu, mips32_instruction_t instruction);
-void sw(r4300i_t* cpu, mips32_instruction_t instruction);
-void ori(r4300i_t* cpu, mips32_instruction_t instruction);
+MIPS32_INSTR(mtc0);
+MIPS32_INSTR(lui);
+MIPS32_INSTR(lw);
+MIPS32_INSTR(sw);
+MIPS32_INSTR(ori);
 
 MIPS32_INSTR(xori);
 
