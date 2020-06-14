6ea88d431fc3d4ca9aada30cc3e30c5006028ae2
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-06-14 12:24:31 -0400

    Debug log lines are green

--- src/common/log.h
+++ src/common/log.h
@@ -7,6 +7,7 @@
 extern unsigned int n64_log_verbosity;
 
 #define COLOR_RED       "\033[0;31m"
+#define COLOR_GREEN     "\033[0;32m"
 #define COLOR_YELLOW    "\033[0;33m"
 #define COLOR_CYAN      "\033[0;36m"
 #define COLOR_END       "\033[0;m"
@@ -26,7 +27,7 @@ extern unsigned int n64_log_verbosity;
 
 #define logwarn(message,...) if (n64_log_verbosity >= LOG_VERBOSITY_WARN) {printf(COLOR_YELLOW "[WARN]  " message "\n" COLOR_END, ##__VA_ARGS__);}
 #define loginfo(message,...) if (n64_log_verbosity >= LOG_VERBOSITY_INFO) {printf(COLOR_CYAN "[INFO]  " message "\n" COLOR_END, ##__VA_ARGS__);}
-#define logdebug(message,...) if (n64_log_verbosity >= LOG_VERBOSITY_DEBUG) {printf("[DEBUG] " message "\n", ##__VA_ARGS__);}
+#define logdebug(message,...) if (n64_log_verbosity >= LOG_VERBOSITY_DEBUG) {printf(COLOR_GREEN "[DEBUG] " message "\n" COLOR_END, ##__VA_ARGS__);}
 #define logtrace(message,...) if (n64_log_verbosity >= LOG_VERBOSITY_TRACE) {printf("[TRACE] " message "\n", ##__VA_ARGS__);}
 
 #define unimplemented(condition, message) if (condition) { logfatal("UNIMPLEMENTED CASE DETECTED: %s", message) }
