a5cea9b366de869469f125af51942233f40d17f5
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-06-15 21:14:27 -0400

    Prefix opcode functions with mips32_

--- src/cpu/mips32.c
+++ src/cpu/mips32.c
@@ -28,58 +28,8 @@ void check_sdword_add_overflow(sdword addend1, sdword addend2, sdword result) {
     }
 }
 
-MIPS32_INSTR(addi) {
-    dword reg_addend = get_register(cpu, instruction.i.rs);
-
-    if (cpu->width_mode == M32) {
-        sword addend1 = sign_extend_word(instruction.i.immediate, 16, 32);
-        sword addend2 = reg_addend;
-        sword result = addend1 + addend2;
-
-        check_sword_add_overflow(addend1, addend2, result);
-
-        set_register(cpu, instruction.i.rt, result);
-        logtrace("Setting r%d to r%d (0x%016lX) + %d (0x%08X) = 0x%016lX",
-                 instruction.i.rt, instruction.i.rs, reg_addend, addend1, addend1, get_register(cpu, instruction.i.rt ))
-    } else if (cpu->width_mode == M64) {
-        sdword addend1 = sign_extend_dword(instruction.i.immediate, 16, 64);
-        sdword addend2 = reg_addend;
-        sdword result = addend1 + addend2;
-
-        check_sdword_add_overflow(addend1, addend2, result);
-
-        set_register(cpu, instruction.i.rt, result);
-        logtrace("Setting r%d to r%d (0x%016lX) + %ld (0x%016lX) = 0x%016lX",
-                 instruction.i.rt, instruction.i.rs, reg_addend, addend1, addend1, get_register(cpu, instruction.i.rt))
-    }
-}
-
-MIPS32_INSTR(addiu) {
-    dword reg_addend = get_register(cpu, instruction.i.rs);
-
-    if (cpu->width_mode == M32) {
-        word addend = sign_extend_word(instruction.i.immediate, 16, 32);
-        set_register(cpu, instruction.i.rt, reg_addend + addend);
-        logtrace("Setting r%d to r%d (0x%016lX) + %d (0x%08X) = 0x%016lX",
-                 instruction.i.rt, instruction.i.rs, reg_addend, addend, addend, get_register(cpu, instruction.i.rt))
-    } else if (cpu->width_mode == M64) {
-        dword addend = sign_extend_dword(instruction.i.immediate, 16, 64);
-        set_register(cpu, instruction.i.rt, reg_addend + addend);
-        logtrace("Setting r%d to r%d (0x%016lX) + %ld (0x%016lX) = 0x%016lX",
-                 instruction.i.rt, instruction.i.rs, reg_addend, addend, addend, get_register(cpu, instruction.i.rt))
-    }
-}
-
-MIPS32_INSTR(andi) {
-    if (cpu->width_mode == M32) {
-        word immediate = instruction.i.immediate;
-        word result = immediate & get_register(cpu, instruction.i.rs);
-        set_register(cpu, instruction.i.rt, result);
-    } else {
-        dword immediate = instruction.i.immediate;
-        dword result = immediate & get_register(cpu, instruction.i.rs);
-        set_register(cpu, instruction.i.rt, result);
-    }
+INLINE void link(r4300i_t* cpu) {
+    set_register(cpu, R4300I_REG_LR, cpu->pc + 4); // Skips the instruction in the delay slot on return
 }
 
 void branch_abs(r4300i_t* cpu, word address) {
@@ -117,44 +67,96 @@ void conditional_branch(r4300i_t* cpu, word offset, bool condition) {
     }
 }
 
-MIPS32_INSTR(beq) {
+
+
+MIPS32_INSTR(mips32_addi) {
+    dword reg_addend = get_register(cpu, instruction.i.rs);
+
+    if (cpu->width_mode == M32) {
+        sword addend1 = sign_extend_word(instruction.i.immediate, 16, 32);
+        sword addend2 = reg_addend;
+        sword result = addend1 + addend2;
+
+        check_sword_add_overflow(addend1, addend2, result);
+
+        set_register(cpu, instruction.i.rt, result);
+        logtrace("Setting r%d to r%d (0x%016lX) + %d (0x%08X) = 0x%016lX",
+                 instruction.i.rt, instruction.i.rs, reg_addend, addend1, addend1, get_register(cpu, instruction.i.rt ))
+    } else if (cpu->width_mode == M64) {
+        sdword addend1 = sign_extend_dword(instruction.i.immediate, 16, 64);
+        sdword addend2 = reg_addend;
+        sdword result = addend1 + addend2;
+
+        check_sdword_add_overflow(addend1, addend2, result);
+
+        set_register(cpu, instruction.i.rt, result);
+        logtrace("Setting r%d to r%d (0x%016lX) + %ld (0x%016lX) = 0x%016lX",
+                 instruction.i.rt, instruction.i.rs, reg_addend, addend1, addend1, get_register(cpu, instruction.i.rt))
+    }
+}
+
+MIPS32_INSTR(mips32_addiu) {
+    dword reg_addend = get_register(cpu, instruction.i.rs);
+
+    if (cpu->width_mode == M32) {
+        word addend = sign_extend_word(instruction.i.immediate, 16, 32);
+        set_register(cpu, instruction.i.rt, reg_addend + addend);
+        logtrace("Setting r%d to r%d (0x%016lX) + %d (0x%08X) = 0x%016lX",
+                 instruction.i.rt, instruction.i.rs, reg_addend, addend, addend, get_register(cpu, instruction.i.rt))
+    } else if (cpu->width_mode == M64) {
+        dword addend = sign_extend_dword(instruction.i.immediate, 16, 64);
+        set_register(cpu, instruction.i.rt, reg_addend + addend);
+        logtrace("Setting r%d to r%d (0x%016lX) + %ld (0x%016lX) = 0x%016lX",
+                 instruction.i.rt, instruction.i.rs, reg_addend, addend, addend, get_register(cpu, instruction.i.rt))
+    }
+}
+
+MIPS32_INSTR(mips32_andi) {
+    if (cpu->width_mode == M32) {
+        word immediate = instruction.i.immediate;
+        word result = immediate & get_register(cpu, instruction.i.rs);
+        set_register(cpu, instruction.i.rt, result);
+    } else {
+        dword immediate = instruction.i.immediate;
+        dword result = immediate & get_register(cpu, instruction.i.rs);
+        set_register(cpu, instruction.i.rt, result);
+    }
+}
+
+MIPS32_INSTR(mips32_beq) {
     conditional_branch(cpu, instruction.i.immediate, get_register(cpu, instruction.i.rs) == get_register(cpu, instruction.i.rt));
 }
 
-void beql(r4300i_t* cpu, mips32_instruction_t instruction) {
+MIPS32_INSTR(mips32_beql) {
     unimplemented(cpu->width_mode == M64, "BEQL in 64bit mode")
     conditional_branch_likely(cpu, instruction.i.immediate, get_register(cpu, instruction.i.rs) == get_register(cpu, instruction.i.rt));
 }
 
-MIPS32_INSTR(ble) {
+MIPS32_INSTR(mips32_ble) {
     conditional_branch(cpu, instruction.i.immediate, get_register(cpu, instruction.i.rs) != get_register(cpu, instruction.i.rt));
 }
 
-MIPS32_INSTR(blezl) {
+MIPS32_INSTR(mips32_blezl) {
     sword reg = get_register(cpu, instruction.i.rs);
     conditional_branch_likely(cpu, instruction.i.immediate, reg <= 0);
 }
 
-MIPS32_INSTR(bne) {
+MIPS32_INSTR(mips32_bne) {
     logtrace("Branch if: 0x%08lX != 0x%08lX", get_register(cpu, instruction.i.rs), get_register(cpu, instruction.i.rt))
     conditional_branch(cpu, instruction.i.immediate, get_register(cpu, instruction.i.rs) != get_register(cpu, instruction.i.rt));
 }
 
-MIPS32_INSTR(bnel) {
+MIPS32_INSTR(mips32_bnel) {
     logtrace("Branch if: 0x%08lX != 0x%08lX", get_register(cpu, instruction.i.rs), get_register(cpu, instruction.i.rt))
     conditional_branch_likely(cpu, instruction.i.immediate, get_register(cpu, instruction.i.rs) != get_register(cpu, instruction.i.rt));
 }
 
 
-MIPS32_INSTR(cache) {
+MIPS32_INSTR(mips32_cache) {
     return; // No need to emulate the cache. Might be fun to do someday for accuracy.
 }
 
-INLINE void link(r4300i_t* cpu) {
-    set_register(cpu, R4300I_REG_LR, cpu->pc + 4); // Skips the instruction in the delay slot on return
-}
-
-MIPS32_INSTR(jal) {
+MIPS32_INSTR(mips32_jal) {
     link(cpu);
     unimplemented(cpu->width_mode == M64, "JAL in 64 bit mode")
 
@@ -165,7 +167,7 @@ MIPS32_INSTR(jal) {
     branch_abs(cpu, target);
 }
 
-MIPS32_INSTR(slti) {
+MIPS32_INSTR(mips32_slti) {
     unimplemented(cpu->width_mode == M64, "SLTI in 64 bit mode")
     sword immediate = sign_extend_word(instruction.i.immediate, 16, 32);
     logtrace("Set if %ld < %d", get_register(cpu, instruction.i.rs), immediate)
@@ -176,13 +178,13 @@ MIPS32_INSTR(slti) {
     }
 }
 
-MIPS32_INSTR(mtc0) {
+MIPS32_INSTR(mips32_mtc0) {
     // TODO: throw a "coprocessor unusuable exception" if CP0 disabled - see manual
     set_cp0_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rt));
     logtrace("Setting CP0 r%d to the value of CPU r%d, which is 0x%016lX", instruction.r.rd, instruction.r.rt, get_register(cpu, instruction.r.rt));
 }
 
-MIPS32_INSTR(lui) {
+MIPS32_INSTR(mips32_lui) {
     word immediate = instruction.i.immediate << 16;
 
     switch (cpu->width_mode) {
@@ -195,7 +197,7 @@ MIPS32_INSTR(lui) {
     }
 }
 
-MIPS32_INSTR(lbu) {
+MIPS32_INSTR(mips32_lbu) {
     shalf offset = instruction.i.immediate;
     word address = get_register(cpu, instruction.i.rs) + offset;
     byte value   = cpu->read_byte(address);
@@ -203,7 +205,7 @@ MIPS32_INSTR(lbu) {
     set_register(cpu, instruction.i.rt, value);
 }
 
-MIPS32_INSTR(lw) {
+MIPS32_INSTR(mips32_lw) {
     shalf offset = instruction.i.immediate;
     word address = get_register(cpu, instruction.i.rs) + offset;
     if ((address & 0b11) > 0) {
@@ -218,7 +220,7 @@ MIPS32_INSTR(lw) {
     set_register(cpu, instruction.i.rt, value);
 }
 
-MIPS32_INSTR(sb) {
+MIPS32_INSTR(mips32_sb) {
     NO64
     shalf offset = instruction.i.immediate;
     word address = get_register(cpu, instruction.i.rs);
@@ -227,7 +229,7 @@ MIPS32_INSTR(sb) {
     cpu->write_byte(address, value);
 }
 
-MIPS32_INSTR(sw) {
+MIPS32_INSTR(mips32_sw) {
     shalf offset = instruction.i.immediate;
     if (cpu->width_mode == M32) {
         word address = get_register(cpu, instruction.i.rs);
@@ -238,42 +240,42 @@ MIPS32_INSTR(sw) {
     }
 }
 
-MIPS32_INSTR(ori) {
+MIPS32_INSTR(mips32_ori) {
     set_register(cpu, instruction.i.rt, instruction.i.immediate | get_register(cpu, instruction.i.rs));
 }
 
-MIPS32_INSTR(xori) {
+MIPS32_INSTR(mips32_xori) {
     set_register(cpu, instruction.i.rt, instruction.i.immediate ^ get_register(cpu, instruction.i.rs));
 }
 
-MIPS32_INSTR(spc_srl) {
+MIPS32_INSTR(mips32_spc_srl) {
     NO64
     set_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rt) >> instruction.r.sa);
 }
 
-MIPS32_INSTR(spc_sllv) {
+MIPS32_INSTR(mips32_spc_sllv) {
     NO64
     set_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rt) << (get_register(cpu, instruction.r.rs) & 0b11111));
 }
 
-MIPS32_INSTR(spc_srlv) {
+MIPS32_INSTR(mips32_spc_srlv) {
     NO64
     set_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rt) >> (get_register(cpu, instruction.r.rs) & 0b11111));
 }
 
-MIPS32_INSTR(spc_jr) {
+MIPS32_INSTR(mips32_spc_jr) {
     branch_abs(cpu, get_register(cpu, instruction.r.rs));
 }
 
-MIPS32_INSTR(spc_mfhi) {
+MIPS32_INSTR(mips32_spc_mfhi) {
     logfatal("mfhi")
 }
 
-MIPS32_INSTR(spc_mflo) {
+MIPS32_INSTR(mips32_spc_mflo) {
     set_register(cpu, instruction.r.rd, cpu->mult_lo);
 }
 
-MIPS32_INSTR(spc_multu) {
+MIPS32_INSTR(mips32_spc_multu) {
     NO64
 
     word multiplicand_1 = get_register(cpu, instruction.r.rs);
@@ -288,7 +290,7 @@ MIPS32_INSTR(spc_multu) {
     cpu->mult_hi = result_upper;
 }
 
-MIPS32_INSTR(spc_add) {
+MIPS32_INSTR(mips32_spc_add) {
     NO64
 
     sword addend1 = get_register(cpu, instruction.r.rs);
@@ -300,36 +302,36 @@ MIPS32_INSTR(spc_add) {
     set_register(cpu, instruction.r.rd, result);
 }
 
-MIPS32_INSTR(spc_addu) {
+MIPS32_INSTR(mips32_spc_addu) {
     NO64
 
     word result = get_register(cpu, instruction.r.rs) + get_register(cpu, instruction.r.rt);
     set_register(cpu, instruction.r.rd, result);
 }
 
-MIPS32_INSTR(spc_and) {
+MIPS32_INSTR(mips32_spc_and) {
     NO64
 
     word result = get_register(cpu, instruction.r.rs) & get_register(cpu, instruction.r.rt);
     set_register(cpu, instruction.r.rd, result);
 }
 
-MIPS32_INSTR(spc_subu) {
+MIPS32_INSTR(mips32_spc_subu) {
     NO64
 
     word result = get_register(cpu, instruction.r.rs) - get_register(cpu, instruction.r.rt);
     set_register(cpu, instruction.r.rd, result);
 }
 
-MIPS32_INSTR(spc_or) {
+MIPS32_INSTR(mips32_spc_or) {
     set_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rs) | get_register(cpu, instruction.r.rt));
 }
 
-MIPS32_INSTR(spc_xor) {
+MIPS32_INSTR(mips32_spc_xor) {
     set_register(cpu, instruction.r.rd, get_register(cpu, instruction.r.rs) ^ get_register(cpu, instruction.r.rt));
 }
 
-MIPS32_INSTR(spc_slt) {
+MIPS32_INSTR(mips32_spc_slt) {
     NO64
 
     sword op1 = get_register(cpu, instruction.r.rs);
@@ -348,7 +350,7 @@ MIPS32_INSTR(spc_slt) {
     }
 }
 
-MIPS32_INSTR(spc_sltu) {
+MIPS32_INSTR(mips32_spc_sltu) {
     NO64
 
     word op1 = get_register(cpu, instruction.r.rs);
@@ -362,12 +364,12 @@ MIPS32_INSTR(spc_sltu) {
     }
 }
 
-MIPS32_INSTR(ri_bgezl) {
+MIPS32_INSTR(mips32_ri_bgezl) {
     sword reg = get_register(cpu, instruction.i.rs);
     conditional_branch_likely(cpu, instruction.i.immediate, reg >= 0);
 }
 
-MIPS32_INSTR(ri_bgezal) {
+MIPS32_INSTR(mips32_ri_bgezal) {
     link(cpu);
     sword reg = get_register(cpu, instruction.i.rs);
     conditional_branch(cpu, instruction.i.immediate, reg >= 0);
--- src/cpu/mips32.h
+++ src/cpu/mips32.h
@@ -4,52 +4,53 @@
 
 #define MIPS32_INSTR(NAME) void NAME(r4300i_t* cpu, mips32_instruction_t instruction)
 
-MIPS32_INSTR(addi);
-MIPS32_INSTR(addiu);
-MIPS32_INSTR(addu);
+MIPS32_INSTR(mips32_addi);
+MIPS32_INSTR(mips32_addiu);
+MIPS32_INSTR(mips32_addu);
 
-MIPS32_INSTR(and);
-MIPS32_INSTR(andi);
+MIPS32_INSTR(mips32_and);
+MIPS32_INSTR(mips32_andi);
 
-MIPS32_INSTR(blezl);
-MIPS32_INSTR(beq);
-MIPS32_INSTR(beql);
-MIPS32_INSTR(bne);
-MIPS32_INSTR(bnel);
+MIPS32_INSTR(mips32_blezl);
+MIPS32_INSTR(mips32_beq);
+MIPS32_INSTR(mips32_beql);
+MIPS32_INSTR(mips32_bne);
+MIPS32_INSTR(mips32_bnel);
 
-MIPS32_INSTR(cache);
+MIPS32_INSTR(mips32_cache);
 
-MIPS32_INSTR(jal);
+MIPS32_INSTR(mips32_jal);
 
-MIPS32_INSTR(slti);
+MIPS32_INSTR(mips32_slti);
 
-MIPS32_INSTR(mtc0);
-MIPS32_INSTR(lui);
-MIPS32_INSTR(lbu);
-MIPS32_INSTR(lw);
-MIPS32_INSTR(sb);
-MIPS32_INSTR(sw);
-MIPS32_INSTR(ori);
+MIPS32_INSTR(mips32_mtc0);
+MIPS32_INSTR(mips32_lui);
+MIPS32_INSTR(mips32_lbu);
+MIPS32_INSTR(mips32_lw);
+MIPS32_INSTR(mips32_sb);
+MIPS32_INSTR(mips32_sw);
+MIPS32_INSTR(mips32_ori);
 
-MIPS32_INSTR(xori);
+MIPS32_INSTR(mips32_xori);
 
-MIPS32_INSTR(spc_srl);
-MIPS32_INSTR(spc_sllv);
-MIPS32_INSTR(spc_srlv);
-MIPS32_INSTR(spc_jr);
-MIPS32_INSTR(spc_mfhi);
-MIPS32_INSTR(spc_mflo);
-MIPS32_INSTR(spc_multu);
-MIPS32_INSTR(spc_add);
-MIPS32_INSTR(spc_addu);
-MIPS32_INSTR(spc_and);
-MIPS32_INSTR(spc_subu);
-MIPS32_INSTR(spc_or);
-MIPS32_INSTR(spc_xor);
-MIPS32_INSTR(spc_slt);
-MIPS32_INSTR(spc_sltu);
+MIPS32_INSTR(mips32_spc_srl);
+MIPS32_INSTR(mips32_spc_sllv);
+MIPS32_INSTR(mips32_spc_srlv);
+MIPS32_INSTR(mips32_spc_jr);
+MIPS32_INSTR(mips32_spc_mfhi);
+MIPS32_INSTR(mips32_spc_mflo);
+MIPS32_INSTR(mips32_spc_multu);
+MIPS32_INSTR(mips32_spc_add);
+MIPS32_INSTR(mips32_spc_addu);
+MIPS32_INSTR(mips32_spc_and);
+MIPS32_INSTR(mips32_spc_subu);
+MIPS32_INSTR(mips32_spc_or);
+MIPS32_INSTR(mips32_spc_xor);
+MIPS32_INSTR(mips32_spc_slt);
+MIPS32_INSTR(mips32_spc_sltu);
 
 
-MIPS32_INSTR(ri_bgezl);
+MIPS32_INSTR(mips32_ri_bgezl);
+MIPS32_INSTR(mips32_ri_bgezal);
 
 #endif //N64_MIPS32_H
