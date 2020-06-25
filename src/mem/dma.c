ff4df4fb22e895f77b54240dae37907c7631fe83
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-06-25 00:45:05 -0400

    Fix DMA

--- src/mem/dma.c
+++ src/mem/dma.c
@@ -6,11 +6,11 @@ bool is_dma_active() {
 }
 
 void run_dma(n64_system_t* system, word source, word dest, word length, const char* direction) {
-    logdebug("DMA requested from 0x%08X to 0x%08X (%s), with a length of %d",
-             source, dest, direction, length)
-    for (int i = 0; i <= length; i++) { // The <= is intentional. The written length value is the length - 1.
+    logdebug("DMA requested at PC 0x%08X from 0x%08X to 0x%08X (%s), with a length of %d",
+             system->cpu.pc, source, dest, direction, length)
+    for (int i = 0; i < length; i++) {
         byte value = n64_read_byte(system, source + i);
-        logtrace("Copying 0x%02X from 0x%08X to 0x%08X", value, source + i, dest + i);
+        logtrace("%s: Copying 0x%02X from 0x%08X to 0x%08X", direction, value, source + i, dest + i);
         n64_write_byte(system, dest + i, value);
     }
     logdebug("DMA completed.")
