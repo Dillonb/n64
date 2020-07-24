24a35468cf1908327da516e2c0383457e7c56e09
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-07-24 08:46:00 -0400

    get sockets set up for gdb stub

---
+++ src/gdbstub/gdbstub.h
@@ -0,0 +1,5 @@
+#ifndef __GDBSTUB_H__
+#define __GDBSTUB_H__
+void gdbstub_init();
+void gdbstub_tick();
+#endif // GDBSTUB_H
--- src/system/n64system.c
+++ src/system/n64system.c
@@ -8,6 +8,7 @@
 #include "../mem/n64_rsp_bus.h"
 #include "../cpu/rsp.h"
 #include "../rdp/rdp.h"
+#include "../gdbstub/gdbstub.h"
 
 // The CPU runs at 93.75mhz. There are 60 frames per second, and 262 lines on the display.
 // There are 1562500 cycles per frame.
@@ -175,10 +176,12 @@ n64_system_t* init_n64system(const char* rom_path, bool enable_frontend) {
     if (enable_frontend) {
         render_init(system);
     }
+    gdbstub_init();
     return system;
 }
 
 INLINE void _n64_system_step(n64_system_t* system) {
+    gdbstub_tick();
     r4300i_step(&system->cpu);
     if (!system->rsp.status.halt) {
         if (++system->rsp.sync >= 3) {
