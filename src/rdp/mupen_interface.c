0ad862cb749a72ad005d70e31d7ca44cc1d1a5f0
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-08-02 09:35:50 -0400

    quiet down plugin interface logging

--- src/rdp/mupen_interface.c
+++ src/rdp/mupen_interface.c
@@ -16,51 +16,41 @@ void init_mupen_interface(n64_system_t* system) {
 }
 
 EXPORT m64p_error CALL ConfigOpenSection(const char* section, m64p_handle* handle) {
-    printf("ConfigOpen %s\n", section);
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL ConfigSaveSection(const char* section) {
-    printf("ConfigSaveSection\n");
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL ConfigSetDefaultInt(m64p_handle handle, const char* param, int value, const char* help) {
-    printf("ConfigSetDefaultInt: %s = %d. Help: %s\n", param, value, help);
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL ConfigSetDefaultFloat(m64p_handle handle, const char* param, float value, const char* help) {
-    printf("ConfigSetDefaultFloat: %s = %f. Help: %s\n", param, value, help);
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL ConfigSetDefaultBool(m64p_handle handle, const char* param, int value, const char* help) {
-    printf("ConfigSetDefaultBool: %s = %d. Help: %s\n", param, value, help);
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL ConfigSetDefaultString(m64p_handle handle, const char* param, const char* value, const char* help) {
-    printf("ConfigSetDefaultString: %s = %s. Help: %s\n", param, value, help);
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL VidExt_Init() {
-    printf("VidExt_Init\n");
     return M64ERR_SUCCESS;
 }
 
 EXPORT m64p_error CALL VidExt_GL_SetAttribute(m64p_GLattr attr, int arg1) {
-    printf("VidExt_GL_SetAttribute\n");
     return M64ERR_SUCCESS;
 }
 EXPORT m64p_error CALL VidExt_SetVideoMode(int width, int height, int bits_per_pixel, m64p_video_mode mode, m64p_video_flags flags) {
-    printf("VidExt_SetVideoMode: width: %d height: %d bpp: %d mode: %d flags: %d\n", width, height, bits_per_pixel, mode, flags);
     return M64ERR_SUCCESS;
 }
 
 EXPORT void* CALL VidExt_GL_GetProcAddress(const char* proc) {
-    printf("VidExt_GL_GetProcAddress: %s\n", proc);
     void* pointer = SDL_GL_GetProcAddress(proc);
     if (pointer == NULL) {
         logfatal("VidExt_GL_GetProcAddress: could not get address of %s!", proc)
@@ -100,7 +90,6 @@ EXPORT const char * CALL ConfigGetParamString(m64p_handle handle, const char* pa
 }
 
 EXPORT m64p_error CALL VidExt_Quit(void) {
-    printf("VidExt_Quit\n");
     return M64ERR_SUCCESS;
 }
 
