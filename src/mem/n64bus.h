78e6d9fb97711390cfddf80885b02f97445c460d
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-09-23 21:16:42 -0400

    little bit faster virtual to physical translation, I hope

--- src/mem/n64bus.h
+++ src/mem/n64bus.h
@@ -9,8 +9,17 @@ bool tlb_probe(word vaddr, word* paddr, int* entry_number, cp0_t* cp0);
 
 INLINE word resolve_virtual_address(word address, cp0_t* cp0) {
     word physical;
-    switch (address) {
-        case VREGION_KUSEG: {
+    switch (address >> 28) {
+
+        // KUSEG
+        case 0x0:
+        case 0x1:
+        case 0x2:
+        case 0x3:
+        case 0x4:
+        case 0x5:
+        case 0x6:
+        case 0x7: {
             if (tlb_probe(address, &physical, NULL, cp0)) {
                 //printf("TLB translation 0x%08x -> 0x%08x\n", address, physical);
             } else {
@@ -18,22 +27,30 @@ INLINE word resolve_virtual_address(word address, cp0_t* cp0) {
             }
             break;
         }
-        case VREGION_KSEG0:
+        // KSEG0
+        case 0x8:
+        case 0x9:
             // Unmapped translation. Subtract the base address of the space to get the physical address.
             physical = address - SVREGION_KSEG0;
             logtrace("KSEG0: Translated 0x%08X to 0x%08X", address, physical);
             break;
-        case VREGION_KSEG1:
+        // KSEG1
+        case 0xA:
+        case 0xB:
             // Unmapped translation. Subtract the base address of the space to get the physical address.
             physical = address - SVREGION_KSEG1;
             logtrace("KSEG1: Translated 0x%08X to 0x%08X", address, physical);
             break;
-        case VREGION_KSSEG:
+        // KSSEG
+        case 0xC:
+        case 0xD:
             logfatal("Unimplemented: translating virtual address in VREGION_KSSEG");
-        case VREGION_KSEG3:
+        // KSEG3
+        case 0xE:
+        case 0xF:
             logfatal("Unimplemented: translating virtual address in VREGION_KSEG3");
         default:
-            logfatal("Address 0x%08X doesn't really look like a virtual address", address);
+            logfatal("PANIC! should never end up here.");
     }
     return physical;
 }
