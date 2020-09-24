38e845db7befc34a9f8a08b78f9ac40168bbfd0d
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-09-24 18:21:34 -0400

    this is compiling as an if/else chain anyways, so make it a little less complex and reorder it in order of most likely branch to be hit

--- src/mem/n64bus.h
+++ src/mem/n64bus.h
@@ -9,45 +9,34 @@ bool tlb_probe(word vaddr, word* paddr, int* entry_number, cp0_t* cp0);
 
 INLINE word resolve_virtual_address(word address, cp0_t* cp0) {
     word physical;
-    switch (address >> 28) {
-
-        // KUSEG
-        case 0x0:
-        case 0x1:
-        case 0x2:
-        case 0x3:
-        case 0x4:
-        case 0x5:
-        case 0x6:
-        case 0x7: {
-            if (tlb_probe(address, &physical, NULL, cp0)) {
-                //printf("TLB translation 0x%08x -> 0x%08x\n", address, physical);
-            } else {
-                logfatal("Unimplemented: page miss translating virtual address 0x%08X in VREGION_KUSEG", address);
-            }
-            break;
-        }
+    switch (address >> 29) {
         // KSEG0
-        case 0x8:
-        case 0x9:
+        case 0x4:
             // Unmapped translation. Subtract the base address of the space to get the physical address.
             physical = address - SVREGION_KSEG0;
             logtrace("KSEG0: Translated 0x%08X to 0x%08X", address, physical);
             break;
-        // KSEG1
-        case 0xA:
-        case 0xB:
+            // KSEG1
+        case 0x5:
             // Unmapped translation. Subtract the base address of the space to get the physical address.
             physical = address - SVREGION_KSEG1;
             logtrace("KSEG1: Translated 0x%08X to 0x%08X", address, physical);
             break;
+        // KUSEG
+        case 0x0:
+        case 0x1:
+        case 0x2:
+        case 0x3: {
+            if (!tlb_probe(address, &physical, NULL, cp0)) {
+                logfatal("Unimplemented: page miss translating virtual address 0x%08X in VREGION_KUSEG", address);
+            }
+            break;
+        }
         // KSSEG
-        case 0xC:
-        case 0xD:
+        case 0x6:
             logfatal("Unimplemented: translating virtual address in VREGION_KSSEG");
         // KSEG3
-        case 0xE:
-        case 0xF:
+        case 0x7:
             logfatal("Unimplemented: translating virtual address in VREGION_KSEG3");
         default:
             logfatal("PANIC! should never end up here.");
