fa7ac0e8e3b5821c2eecebac24d716710416e6ef
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-11-28 13:13:33 -0500

    gitignore, remove print statement

--- src/gtk_frontend/gtk_frontend.c
+++ src/gtk_frontend/gtk_frontend.c
@@ -14,8 +14,8 @@ int main(int argc, char** argv) {
     app = gtk_application_new("com.dillonbeliveau.n64", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
-    printf("passed it\n");
     g_object_unref(app);
+
 
     return status;
 }
