854805a585394545c0776729ff1ff7a011505dcd
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-12-28 18:24:06 -0500

    ignore writes to cart_2_1

--- src/mem/n64bus.c
+++ src/mem/n64bus.c
@@ -926,7 +926,8 @@ void n64_write_byte(n64_system_t* system, word address, byte value) {
         case REGION_UNUSED:
             logfatal("Writing byte 0x%02X to address 0x%08X in unsupported region: REGION_UNUSED", value, address);
         case REGION_CART_2_1:
-            logfatal("Writing byte 0x%02X to address 0x%08X in unsupported region: REGION_CART_2_1", value, address);
+            logwarn("Writing byte 0x%02X to address 0x%08X in unsupported region: REGION_CART_2_1", value, address);
+            return;
         case REGION_CART_1_1:
             logfatal("Writing byte 0x%02X to address 0x%08X in unsupported region: REGION_CART_1_1", value, address);
         case REGION_CART_2_2:
