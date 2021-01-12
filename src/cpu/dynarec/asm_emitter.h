380ba7f7c06179969bdd95a54fa0d989768f0d60
Dillon Beliveau <dillonbeliveau@gmail.com>
2021-01-11 22:05:37 -0500

    rest of compile logic extracted to dynarec.c

--- src/cpu/dynarec/asm_emitter.h
+++ src/cpu/dynarec/asm_emitter.h
@@ -3,7 +3,11 @@
 
 #include "dynarec.h"
 #include <system/n64system.h>
+#include <dynasm/dasm_proto.h>
 
-void compile_new_block(n64_dynarec_t* dynarec, r4300i_t* compile_time_cpu, n64_dynarec_block_t* block, word virtual_address, word physical_address);
-
+dasm_State* block_header();
+void advance_pc(r4300i_t* compile_time_cpu, dasm_State** Dst);
+dynarec_instruction_category_t compile_instruction(dasm_State** Dst, mips_instruction_t instr, word address, word block_length, word* extra_cycles);
+void end_block(dasm_State** Dst, int block_length);
+void end_block_early_on_branch_taken(dasm_State** Dst, int block_length);
 #endif //N64_ASM_EMITTER_H
