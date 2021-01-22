ccdb94a97ef237eff2aeb88432f8b10569af436d
Dillon Beliveau <dillonbeliveau@gmail.com>
2021-01-21 23:21:18 -0500

    use a renderpass, draw parallel-rdp's image to swapchain using a textured quad

--- src/rdp/parallel_rdp_wrapper.cpp
+++ src/rdp/parallel_rdp_wrapper.cpp
@@ -111,6 +111,147 @@ public:
     }
 };
 
+/*
+#version 450
+layout(location = 0) in vec2 Position;
+layout(location = 0) out highp vec2 vUV;
+
+void main()
+{
+    gl_Position = vec4(Position, 0.0, 1.0);
+    vUV = 0.5 * Position + 0.5;
+}
+*/
+uint32_t fullscreen_quad_vert[] = {0x07230203,0x00010000,0x000d000a,0x00000022,
+                                   0x00000000,0x00020011,0x00000001,0x0006000b,
+                                   0x00000001,0x4c534c47,0x6474732e,0x3035342e,
+                                   0x00000000,0x0003000e,0x00000000,0x00000001,
+                                   0x0008000f,0x00000000,0x00000004,0x6e69616d,
+                                   0x00000000,0x0000000d,0x00000012,0x0000001c,
+                                   0x00030003,0x00000002,0x000001c2,0x000a0004,
+                                   0x475f4c47,0x4c474f4f,0x70635f45,0x74735f70,
+                                   0x5f656c79,0x656e696c,0x7269645f,0x69746365,
+                                   0x00006576,0x00080004,0x475f4c47,0x4c474f4f,
+                                   0x6e695f45,0x64756c63,0x69645f65,0x74636572,
+                                   0x00657669,0x00040005,0x00000004,0x6e69616d,
+                                   0x00000000,0x00060005,0x0000000b,0x505f6c67,
+                                   0x65567265,0x78657472,0x00000000,0x00060006,
+                                   0x0000000b,0x00000000,0x505f6c67,0x7469736f,
+                                   0x006e6f69,0x00070006,0x0000000b,0x00000001,
+                                   0x505f6c67,0x746e696f,0x657a6953,0x00000000,
+                                   0x00070006,0x0000000b,0x00000002,0x435f6c67,
+                                   0x4470696c,0x61747369,0x0065636e,0x00070006,
+                                   0x0000000b,0x00000003,0x435f6c67,0x446c6c75,
+                                   0x61747369,0x0065636e,0x00030005,0x0000000d,
+                                   0x00000000,0x00050005,0x00000012,0x69736f50,
+                                   0x6e6f6974,0x00000000,0x00030005,0x0000001c,
+                                   0x00565576,0x00050048,0x0000000b,0x00000000,
+                                   0x0000000b,0x00000000,0x00050048,0x0000000b,
+                                   0x00000001,0x0000000b,0x00000001,0x00050048,
+                                   0x0000000b,0x00000002,0x0000000b,0x00000003,
+                                   0x00050048,0x0000000b,0x00000003,0x0000000b,
+                                   0x00000004,0x00030047,0x0000000b,0x00000002,
+                                   0x00040047,0x00000012,0x0000001e,0x00000000,
+                                   0x00040047,0x0000001c,0x0000001e,0x00000000,
+                                   0x00020013,0x00000002,0x00030021,0x00000003,
+                                   0x00000002,0x00030016,0x00000006,0x00000020,
+                                   0x00040017,0x00000007,0x00000006,0x00000004,
+                                   0x00040015,0x00000008,0x00000020,0x00000000,
+                                   0x0004002b,0x00000008,0x00000009,0x00000001,
+                                   0x0004001c,0x0000000a,0x00000006,0x00000009,
+                                   0x0006001e,0x0000000b,0x00000007,0x00000006,
+                                   0x0000000a,0x0000000a,0x00040020,0x0000000c,
+                                   0x00000003,0x0000000b,0x0004003b,0x0000000c,
+                                   0x0000000d,0x00000003,0x00040015,0x0000000e,
+                                   0x00000020,0x00000001,0x0004002b,0x0000000e,
+                                   0x0000000f,0x00000000,0x00040017,0x00000010,
+                                   0x00000006,0x00000002,0x00040020,0x00000011,
+                                   0x00000001,0x00000010,0x0004003b,0x00000011,
+                                   0x00000012,0x00000001,0x0004002b,0x00000006,
+                                   0x00000014,0x00000000,0x0004002b,0x00000006,
+                                   0x00000015,0x3f800000,0x00040020,0x00000019,
+                                   0x00000003,0x00000007,0x00040020,0x0000001b,
+                                   0x00000003,0x00000010,0x0004003b,0x0000001b,
+                                   0x0000001c,0x00000003,0x0004002b,0x00000006,
+                                   0x0000001d,0x3f000000,0x00050036,0x00000002,
+                                   0x00000004,0x00000000,0x00000003,0x000200f8,
+                                   0x00000005,0x0004003d,0x00000010,0x00000013,
+                                   0x00000012,0x00050051,0x00000006,0x00000016,
+                                   0x00000013,0x00000000,0x00050051,0x00000006,
+                                   0x00000017,0x00000013,0x00000001,0x00070050,
+                                   0x00000007,0x00000018,0x00000016,0x00000017,
+                                   0x00000014,0x00000015,0x00050041,0x00000019,
+                                   0x0000001a,0x0000000d,0x0000000f,0x0003003e,
+                                   0x0000001a,0x00000018,0x0004003d,0x00000010,
+                                   0x0000001e,0x00000012,0x0005008e,0x00000010,
+                                   0x0000001f,0x0000001e,0x0000001d,0x00050050,
+                                   0x00000010,0x00000020,0x0000001d,0x0000001d,
+                                   0x00050081,0x00000010,0x00000021,0x0000001f,
+                                   0x00000020,0x0003003e,0x0000001c,0x00000021,
+                                   0x000100fd,0x00010038};
+
+
+/*
+#version 450
+layout(location = 0) in vec2 vUV;
+layout(location = 0) out vec4 FragColor;
+layout(set = 0, binding = 0) uniform sampler2D uImage;
+
+layout(constant_id = 0) const float Scale = 1.0;
+
+void main()
+{
+    FragColor = Scale * textureLod(uImage, vUV, 0.0);
+}
+*/
+static uint32_t fullscreen_quad_blit[] = {0x07230203,0x00010000,0x000d000a,0x00000017,
+                                          0x00000000,0x00020011,0x00000001,0x0006000b,
+                                          0x00000001,0x4c534c47,0x6474732e,0x3035342e,
+                                          0x00000000,0x0003000e,0x00000000,0x00000001,
+                                          0x0007000f,0x00000004,0x00000004,0x6e69616d,
+                                          0x00000000,0x00000009,0x00000012,0x00030010,
+                                          0x00000004,0x00000007,0x00030003,0x00000002,
+                                          0x000001c2,0x000a0004,0x475f4c47,0x4c474f4f,
+                                          0x70635f45,0x74735f70,0x5f656c79,0x656e696c,
+                                          0x7269645f,0x69746365,0x00006576,0x00080004,
+                                          0x475f4c47,0x4c474f4f,0x6e695f45,0x64756c63,
+                                          0x69645f65,0x74636572,0x00657669,0x00040005,
+                                          0x00000004,0x6e69616d,0x00000000,0x00050005,
+                                          0x00000009,0x67617246,0x6f6c6f43,0x00000072,
+                                          0x00040005,0x0000000a,0x6c616353,0x00000065,
+                                          0x00040005,0x0000000e,0x616d4975,0x00006567,
+                                          0x00030005,0x00000012,0x00565576,0x00040047,
+                                          0x00000009,0x0000001e,0x00000000,0x00040047,
+                                          0x0000000a,0x00000001,0x00000000,0x00040047,
+                                          0x0000000e,0x00000022,0x00000000,0x00040047,
+                                          0x0000000e,0x00000021,0x00000000,0x00040047,
+                                          0x00000012,0x0000001e,0x00000000,0x00020013,
+                                          0x00000002,0x00030021,0x00000003,0x00000002,
+                                          0x00030016,0x00000006,0x00000020,0x00040017,
+                                          0x00000007,0x00000006,0x00000004,0x00040020,
+                                          0x00000008,0x00000003,0x00000007,0x0004003b,
+                                          0x00000008,0x00000009,0x00000003,0x00040032,
+                                          0x00000006,0x0000000a,0x3f800000,0x00090019,
+                                          0x0000000b,0x00000006,0x00000001,0x00000000,
+                                          0x00000000,0x00000000,0x00000001,0x00000000,
+                                          0x0003001b,0x0000000c,0x0000000b,0x00040020,
+                                          0x0000000d,0x00000000,0x0000000c,0x0004003b,
+                                          0x0000000d,0x0000000e,0x00000000,0x00040017,
+                                          0x00000010,0x00000006,0x00000002,0x00040020,
+                                          0x00000011,0x00000001,0x00000010,0x0004003b,
+                                          0x00000011,0x00000012,0x00000001,0x0004002b,
+                                          0x00000006,0x00000014,0x00000000,0x00050036,
+                                          0x00000002,0x00000004,0x00000000,0x00000003,
+                                          0x000200f8,0x00000005,0x0004003d,0x0000000c,
+                                          0x0000000f,0x0000000e,0x0004003d,0x00000010,
+                                          0x00000013,0x00000012,0x00070058,0x00000007,
+                                          0x00000015,0x0000000f,0x00000013,0x00000002,
+                                          0x00000014,0x0005008e,0x00000007,0x00000016,
+                                          0x00000015,0x0000000a,0x0003003e,0x00000009,
+                                          0x00000016,0x000100fd,0x00010038};
+
+Program* fullscreen_quad_program;
+
 void load_parallel_rdp(n64_system_t* system) {
     wsi = new WSI();
     wsi->set_backbuffer_srgb(false);
@@ -118,6 +259,8 @@ void load_parallel_rdp(n64_system_t* system) {
     if (!wsi->init(1, nullptr)) {
         logfatal("Failed to initialize WSI!");
     }
+
+    fullscreen_quad_program = wsi->get_device().request_program(fullscreen_quad_vert, sizeof(fullscreen_quad_vert), fullscreen_quad_blit, sizeof(fullscreen_quad_blit));
 
     auto aligned_rdram = reinterpret_cast<uintptr_t>(system->mem.rdram);
     uintptr_t offset = 0;
@@ -141,11 +284,30 @@ void load_parallel_rdp(n64_system_t* system) {
     }
 }
 
+void draw_fullscreen_textured_quad(Util::IntrusivePtr<Image> image, Util::IntrusivePtr<CommandBuffer> cmd) {
+    cmd->set_texture(0, 0, image->get_view(), Vulkan::StockSampler::LinearClamp);
+    cmd->set_program(fullscreen_quad_program);
+    cmd->set_quad_state();
+    auto *data = static_cast<float *>(cmd->allocate_vertex_data(0, 6 * sizeof(float), 2 * sizeof(float)));
+    *data++ = -1.0f;
+    *data++ = -3.0f;
+    *data++ = -1.0f;
+    *data++ = +1.0f;
+    *data++ = +3.0f;
+    *data++ = +1.0f;
+
+    cmd->set_vertex_attrib(0, 0, VK_FORMAT_R32G32_SFLOAT, 0);
+    cmd->set_depth_test(false, false);
+    cmd->set_depth_compare(VK_COMPARE_OP_ALWAYS);
+    cmd->set_primitive_topology(VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+    cmd->draw(3, 1);
+}
+
 void update_screen(Util::IntrusivePtr<Image> image) {
     wsi->begin_frame();
 
     if (!image) {
-        auto info = Vulkan::ImageCreateInfo::immutable_2d_image(1, 1, VK_FORMAT_R8G8B8A8_UNORM);
+        auto info = Vulkan::ImageCreateInfo::immutable_2d_image(N64_SCREEN_X * SCREEN_SCALE, N64_SCREEN_Y * SCREEN_SCALE, VK_FORMAT_R8G8B8A8_UNORM);
         info.usage = VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
                      VK_IMAGE_USAGE_TRANSFER_DST_BIT;
         info.misc = IMAGE_MISC_MUTABLE_SRGB_BIT;
@@ -166,33 +328,12 @@ void update_screen(Util::IntrusivePtr<Image> image) {
         wsi->get_device().submit(cmd);
     }
 
-    auto cmd = wsi->get_device().request_command_buffer();
+    Util::IntrusivePtr<CommandBuffer> cmd = wsi->get_device().request_command_buffer();
 
-    Image& swapchain_image = wsi->get_device().get_swapchain_view().get_image();
-    VkOffset3D dst_extent = {int(swapchain_image.get_width()), int(swapchain_image.get_height()), 1};
-    VkOffset3D src_extent = {int(image->get_width()),          int(image->get_height()),          1};
-
-    cmd->image_barrier(swapchain_image, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
-                       VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, 0,
-                       VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_WRITE_BIT);
-
-    Image& image_ref = image->get_view().get_image();
-
-    cmd->image_barrier(image_ref, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
-                       VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, 0,
-                       VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_READ_BIT);
-
-    cmd->blit_image(swapchain_image, image_ref,
-                    {}, dst_extent,
-                    {}, src_extent,
-                    0, 0);
-
-    cmd->image_barrier(swapchain_image, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
-                       VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, 0,
-                       VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_READ_BIT);
-
+    cmd->begin_render_pass(wsi->get_device().get_swapchain_render_pass(SwapchainRenderPass::ColorOnly));
+    draw_fullscreen_textured_quad(image, cmd);
     ImGui_ImplVulkan_RenderDrawData(imgui_frame(), cmd->get_command_buffer());
-    cmd->uses_swapchain = true;
+    cmd->end_render_pass();
     wsi->get_device().submit(cmd);
     wsi->end_frame();
 }
