486e9f483c559c4e410d46833674f60841f82a3e
Dillon Beliveau <dillonbeliveau@gmail.com>
2021-05-09 11:29:35 -0400

    Fix keyboard joystick Y axis being reversed

--- src/frontend/frontend.c
+++ src/frontend/frontend.c
@@ -29,13 +29,13 @@ void handle_event(SDL_Event* event) {
 
                 case SDLK_UP:
                 case SDLK_w:
-                    update_joyaxis_y(0, JOYAXIS_MAX);
+                    update_joyaxis_y(0, JOYAXIS_MIN);
                     //update_button(0, DPAD_UP, true);
                     break;
 
                 case SDLK_DOWN:
                 case SDLK_s:
-                    update_joyaxis_y(0, JOYAXIS_MIN);
+                    update_joyaxis_y(0, JOYAXIS_MAX);
                     //update_button(0, DPAD_DOWN, true);
                     break;
 
