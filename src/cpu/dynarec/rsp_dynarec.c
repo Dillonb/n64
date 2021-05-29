36b507609625ca5882d03fcd1de228a22fb377a2
Dillon Beliveau <dillonbeliveau@gmail.com>
2021-05-29 19:44:23 -0400

    fixes

--- src/cpu/dynarec/rsp_dynarec.c
+++ src/cpu/dynarec/rsp_dynarec.c
@@ -4,11 +4,11 @@
 #include "asm_emitter.h"
 #include "dynarec_memory_management.h"
 
-static void* link_and_encode(dasm_State** d) {
+void* rsp_link_and_encode(dasm_State** d) {
     size_t code_size;
     dasm_link(d, &code_size);
 #ifdef N64_LOG_COMPILATIONS
-    printf("Generated %ld bytes of code\n", code_size);
+    printf("Generated %ld bytes of RSP code\n", code_size);
 #endif
     void* buf = rsp_dynarec_bumpalloc(code_size);
     dasm_encode(d, buf);
@@ -18,21 +18,24 @@ static void* link_and_encode(dasm_State** d) {
 
 void compile_new_rsp_block(rsp_dynarec_block_t* block, word address) {
     static dasm_State* d;
+    static dasm_State** Dst;
+
     d = block_header();
-    dasm_State** Dst = &d;
+    Dst = &d;
 
     int block_length = 0;
     int block_extra_cycles = 0;
     bool should_continue_block = true;
+    word extra_cycles = 0;
 
     do {
         static mips_instruction_t instr;
         instr.raw = word_from_byte_array(N64RSP.sp_imem, address);
 
         dynarec_ir_t* ir = rsp_instruction_ir(instr, address);
-        word extra_cycles = 0;
         advance_rsp_pc(Dst);
         ir->compiler(Dst, instr, address, NULL, 0, &extra_cycles);
+        block_length++;
         block_extra_cycles += extra_cycles;
 
         // limit ourselves to one instruction per block for now
@@ -40,13 +43,13 @@ void compile_new_rsp_block(rsp_dynarec_block_t* block, word address) {
     } while(should_continue_block);
 
     end_rsp_block(Dst, block_length + block_extra_cycles);
-    void* compiled = link_and_encode(&d);
-    dasm_free(&d);
+    void* compiled = rsp_link_and_encode(Dst);
+    dasm_free(Dst);
 
     block->run = compiled;
 }
 
-static int missing_block_handler() {
+int rsp_missing_block_handler() {
     word pc = N64RSP.pc & 0x3FF;
     rsp_dynarec_block_t* block = &N64RSPDYNAREC->blockcache[pc];
     compile_new_rsp_block(block, N64RSP.pc << 2);
@@ -60,7 +63,7 @@ rsp_dynarec_t* rsp_dynarec_init(byte* codecache, size_t codecache_size) {
     dynarec->codecache_used = 0;
 
     for (int i = 0; i < RSP_BLOCKCACHE_SIZE; i++) {
-        dynarec->blockcache[i].run = missing_block_handler;
+        dynarec->blockcache[i].run = rsp_missing_block_handler;
     }
 
     dynarec->codecache = codecache;
@@ -72,7 +75,7 @@ int rsp_dynarec_step() {
     rsp_dynarec_block_t* block = &N64RSPDYNAREC->blockcache[N64RSP.pc & 0x3FF];
     // temporary...
     if (block->run == NULL) {
-        block->run = missing_block_handler;
+        block->run = rsp_missing_block_handler;
     }
-    block->run(&N64RSP);
+    return block->run(&N64RSP);
 }
