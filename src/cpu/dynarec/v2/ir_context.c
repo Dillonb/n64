6a224a1639a4cbc5bafeda151290654644ac2ad3
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-01-22 15:26:16 -0800

    don't load extra zero constants

--- src/cpu/dynarec/v2/ir_context.c
+++ src/cpu/dynarec/v2/ir_context.c
@@ -5,26 +5,62 @@
 ir_context_t ir_context;
 
 void ir_context_reset() {
-    ir_context.ir_cache_index = 0;
     for (int i = 0; i < 32; i++) {
         ir_context.guest_gpr_to_value[i] = -1;
     }
 
     memset(ir_context.ir_cache, 0, sizeof(ir_instruction_t) * IR_CACHE_SIZE);
+
+    ir_context.ir_cache[0].type = IR_SET_CONSTANT;
+    ir_context.ir_cache[0].set_constant.type = VALUE_TYPE_64;
+    ir_context.ir_cache[0].set_constant.value_64 = 0;
+
+    ir_context.ir_cache_index = 1;
+}
+
+
+void update_guest_reg_mapping(u8 guest_reg, int index) {
+    if (guest_reg < 32) {
+        ir_context.guest_gpr_to_value[guest_reg] = index;
+    }
 }
 
 int append_ir_instruction(ir_instruction_t instruction, u8 guest_reg) {
     int index = ir_context.ir_cache_index++;
     ir_context.ir_cache[index] = instruction;
-    if (guest_reg < 32) {
-        ir_context.guest_gpr_to_value[guest_reg] = index;
-    }
+    update_guest_reg_mapping(guest_reg, index);
     return index;
 }
 
 int ir_emit_set_constant(ir_set_constant_t value, u8 guest_reg) {
     if (guest_reg == 0) {
-        return -1;
+        // v0 is always zero, don't emit anything
+        return 0;
+    }
+
+    bool is_zero = false;
+    switch (value.type) {
+        case VALUE_TYPE_S16:
+            is_zero = value.value_s16 == 0;
+            break;
+        case VALUE_TYPE_U16:
+            is_zero = value.value_u16 == 0;
+            break;
+        case VALUE_TYPE_S32:
+            logfatal("Set constant S32");
+            //is_zero = value.value_s32 == 0;
+            break;
+        case VALUE_TYPE_U32:
+            logfatal("Set constant U32");
+            //is_zero = value.value_u32 == 0;
+            break;
+        case VALUE_TYPE_64:
+            is_zero = value.value_64 == 0;
+            break;
+    }
+    if (is_zero) {
+        update_guest_reg_mapping(guest_reg, 0);
+        return 0;
     }
 
     ir_instruction_t instruction;
