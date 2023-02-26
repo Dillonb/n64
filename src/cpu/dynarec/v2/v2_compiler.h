11dbb2be392158b9dae0f3e38f967a0e72bef050
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-02-25 17:50:34 -0800

    print_ir_block in header

--- src/cpu/dynarec/v2/v2_compiler.h
+++ src/cpu/dynarec/v2/v2_compiler.h
@@ -3,6 +3,7 @@
 
 #include <dynarec/dynarec.h>
 
+void print_ir_block();
 void v2_compile_new_block(n64_dynarec_block_t *block, bool *code_mask, u64 virtual_address, u32 physical_address);
 void v2_compiler_init();
 
