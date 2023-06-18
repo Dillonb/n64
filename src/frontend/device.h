660a0dc0ff90090c9d32bffc63c9e68bcdeef197
kev4cards <21962903+kev4cards@users.noreply.github.com>
2023-06-17 21:48:57 -0400

    Add deadzone_corrected_response to device.h
    also change trim_gamepad_axis to return type double

--- src/frontend/device.h
+++ src/frontend/device.h
@@ -108,9 +108,11 @@ n64_controller_accessory_type_t get_controller_accessory_type(int pif_channel);
 
 // Exposed for testing
 
-// Trim and apply deadzone
-s8 trim_gamepad_axis(s16 raw);
-// do all requisite clamping for a controller
+// Trim input
+double trim_gamepad_axis(s16 raw);
+// Apply deadzone and scale according to response curve
+double deadzone_corrected_response(double peak_cardinal, double peak_diagonal, double axial_deadzone, double outer_radius, double length, double current_axis_input);
+// Do all requisite clamping for a controller
 void clamp_gamepad(n64_controller_t* controller);
 
 #ifdef __cplusplus
