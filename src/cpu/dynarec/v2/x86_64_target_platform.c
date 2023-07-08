c122f9df3eaaad286bec5e0bfc7da9a5cd451da5
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-07-08 18:03:29 -0400

    Windows support for dynarec v2 using the MS ABI

--- src/cpu/dynarec/v2/x86_64_target_platform.c
+++ src/cpu/dynarec/v2/x86_64_target_platform.c
@@ -11,6 +11,19 @@ int get_num_fgrs() {
 
 const int* get_preserved_gprs() {
     const static int preserved_regs[] = {
+#ifdef N64_WIN
+            REG_RBX,
+            REG_RBP,
+            REG_RDI,
+            REG_RSI,
+            // Yes, it's preserved, but we can't use it for register allocation.
+            //REG_RSP,
+            // Holds the CPU state, can't use for register allocation.
+            //REG_R12,
+            REG_R13,
+            REG_R14,
+            REG_R15
+#else
             REG_RBX,
             // Yes, it's preserved, but we can't use it for register allocation.
             //REG_RSP,
@@ -20,24 +33,37 @@ const int* get_preserved_gprs() {
             REG_R13,
             REG_R14,
             REG_R15
+#endif // N64_WIN
     };
 
     return preserved_regs;
 }
 
 int get_num_preserved_gprs() {
+#ifdef N64_WIN
+    return 7; // 9 if we include the stack pointer and r12, but we can't.
+#else
     return 5; // 7 if we include the stack pointer and r12, but we can't.
+#endif // N64_WIN
 }
 
 const int* get_scratch_registers() {
     const static int scratch_regs[] = {
+#ifdef N64_WIN
+            REG_RAX, REG_RCX, REG_RDX, REG_R8, REG_R9, REG_R10, REG_R11
+#else
             REG_RAX, REG_RDI, REG_RSI, REG_RDX, REG_RCX, REG_R8, REG_R9, REG_R10, REG_R11
+#endif // N64_WIN
     };
     return scratch_regs;
 }
 
 int get_num_scratch_registers() {
+#ifdef N64_WIN
+    return 7;
+#else
     return 9;
+#endif // N64_WIN
 }
 
 bool is_valid_immediate(ir_value_type_t value_type) {
@@ -58,18 +84,29 @@ bool is_valid_immediate(ir_value_type_t value_type) {
 
 const int* get_func_arg_registers() {
     const static int func_arg_regs[] = {
+#ifdef N64_WIN
+            REG_RCX,
+            REG_RDX,
+            REG_R8,
+            REG_R9
+#else
             REG_RDI,
             REG_RSI,
             REG_RDX,
             REG_RCX,
             REG_R8,
             REG_R9
+#endif // N64_WIN
     };
     return func_arg_regs;
 }
 
 int get_num_func_arg_registers() {
+#ifdef N64_WIN
+    return 4;
+#else
     return 6;
+#endif // N64_WIN
 }
 
 int get_return_value_reg() {
