984f739f7780167e4299f76df2df78b02ec6bd11
Dillon Beliveau <dillonbeliveau@gmail.com>
2023-09-10 15:42:56 -0700

    improve LQV, fix disassembly of LWC2/SWC2

--- src/cpu/rsp_disassemble.c
+++ src/cpu/rsp_disassemble.c
@@ -9,17 +9,17 @@ int disassemble_rsp_cp2(u32 address, mips_instruction_t instr, char* buf, int bu
 
 int disassemble_rsp_lwc2(u32 address, mips_instruction_t instr, char* buf, int buflen) {
     switch (instr.v.funct) {
-        case LWC2_LBV: snprintf(buf, buflen, "lbv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LDV: snprintf(buf, buflen, "ldv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LFV: snprintf(buf, buflen, "lfv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LHV: snprintf(buf, buflen, "lhv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LLV: snprintf(buf, buflen, "llv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LPV: snprintf(buf, buflen, "lpv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LQV: snprintf(buf, buflen, "lqv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LRV: snprintf(buf, buflen, "lrv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LSV: snprintf(buf, buflen, "lsv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LTV: snprintf(buf, buflen, "ltv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LUV: snprintf(buf, buflen, "luv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
+        case LWC2_LBV: snprintf(buf, buflen, "lbv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LDV: snprintf(buf, buflen, "ldv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LFV: snprintf(buf, buflen, "lfv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LHV: snprintf(buf, buflen, "lhv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LLV: snprintf(buf, buflen, "llv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LPV: snprintf(buf, buflen, "lpv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LQV: snprintf(buf, buflen, "lqv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LRV: snprintf(buf, buflen, "lrv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LSV: snprintf(buf, buflen, "lsv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LTV: snprintf(buf, buflen, "ltv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LUV: snprintf(buf, buflen, "luv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
         case SWC2_SWV: snprintf(buf, buflen, "swv (rsp_nop)"); break;
         default:
             return 0;
@@ -29,18 +29,18 @@ int disassemble_rsp_lwc2(u32 address, mips_instruction_t instr, char* buf, int b
 
 int disassemble_rsp_swc2(u32 address, mips_instruction_t instr, char* buf, int buflen) {
     switch (instr.v.funct) {
-        case LWC2_LBV: snprintf(buf, buflen, "sbv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LDV: snprintf(buf, buflen, "sdv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LFV: snprintf(buf, buflen, "sfv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LHV: snprintf(buf, buflen, "shv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LLV: snprintf(buf, buflen, "slv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LPV: snprintf(buf, buflen, "spv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LQV: snprintf(buf, buflen, "sqv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LRV: snprintf(buf, buflen, "srv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LSV: snprintf(buf, buflen, "ssv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LTV: snprintf(buf, buflen, "stv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case LWC2_LUV: snprintf(buf, buflen, "suv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
-        case SWC2_SWV: snprintf(buf, buflen, "swv $v%d[%d], %02X($v%d)", instr.v.vt, instr.v.element, instr.v.offset, instr.v.base); break;
+        case LWC2_LBV: snprintf(buf, buflen, "sbv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LDV: snprintf(buf, buflen, "sdv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LFV: snprintf(buf, buflen, "sfv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LHV: snprintf(buf, buflen, "shv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LLV: snprintf(buf, buflen, "slv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LPV: snprintf(buf, buflen, "spv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LQV: snprintf(buf, buflen, "sqv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LRV: snprintf(buf, buflen, "srv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LSV: snprintf(buf, buflen, "ssv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LTV: snprintf(buf, buflen, "stv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case LWC2_LUV: snprintf(buf, buflen, "suv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
+        case SWC2_SWV: snprintf(buf, buflen, "swv $v%d[%d], %02X($%s)", instr.v.vt, instr.v.element, instr.v.offset, register_names[instr.v.base]); break;
         default:
             return 0;
     }
