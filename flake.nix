75705c84f578c74e5813839728b6e01d4300225c
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-03 15:57:49 -0700

    Redo Flake using flake-utils to clean up a bit

--- flake.nix
+++ flake.nix
@@ -3,104 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, systems }:
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
     let
-      eachSystem = nixpkgs.lib.genAttrs (import systems);
       shortRev = with self; if sourceInfo?dirtyShortRev then sourceInfo.dirtyShortRev else sourceInfo.shortRev;
       rev = with self; if sourceInfo?dirtyRev then sourceInfo.dirtyRev else sourceInfo.rev;
-      get_deps = (pkgs:
-        let
-          llvmPackage = pkgs.llvmPackages_18;
-          libcxx = llvmPackage.libraries.libcxx;
-          clang = llvmPackage.libcxxClang;
-        in
-        {
-          clang = clang;
-          libcxx = libcxx;
-          tools = [
-            pkgs.cmake
-            clang
-            pkgs.ninja
-            pkgs.shaderc
-            pkgs.pkg-config
-            pkgs.vulkan-loader
-            pkgs.mold-wrapped
-          ];
+      pkgs = import nixpkgs { inherit system; };
+      llvmPackage = pkgs.llvmPackages_18;
+      libcxx = llvmPackage.libraries.libcxx;
+      clang = llvmPackage.libcxxClang;
+      tools = [
+        pkgs.cmake
+        clang
+        pkgs.ninja
+        pkgs.shaderc
+        pkgs.pkg-config
+        pkgs.vulkan-loader
+        pkgs.mold-wrapped
+      ];
 
-          devShellTools = [
-            pkgs.gdb
-          ];
+      devShellTools = [
+        pkgs.gdb
+      ];
 
-          libs = [
-            pkgs.SDL2
-            pkgs.capstone
-            pkgs.dbus
-            pkgs.bzip2
-            libcxx
-          ];
-        });
+      libs = [
+        pkgs.SDL2
+        pkgs.capstone
+        pkgs.dbus
+        pkgs.bzip2
+        libcxx
+      ];
+
+      lib_cpath = pkgs.lib.makeSearchPathOutput "dev" "include" (libs);
+      # Taken from CMake's ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}
+      extra_cpath = "${libcxx.dev}/include/c++/v1:${clang}/resource-root/include:${pkgs.glibc.dev}/include";
     in
     {
-      devShells = eachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            deps = (get_deps pkgs);
-            lib_cpath = pkgs.lib.makeSearchPathOutput "dev" "include" (deps.libs);
-            # Taken from CMake's ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}
-            extra_cpath = "${deps.libcxx.dev}/include/c++/v1:${deps.clang}/resource-root/include:${pkgs.glibc.dev}/include";
-          in
-          {
-            default = pkgs.mkShell
-              {
-                buildInputs = deps.tools ++ deps.libs ++ deps.devShellTools;
-                CPATH = "${lib_cpath}:${extra_cpath}";
-                LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
-              };
-          });
-
-      packages = eachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-            deps = (get_deps pkgs);
-          in
-          {
-            default = pkgs.stdenv.mkDerivation
-              {
-                pname = "dgb-n64";
-                version = "0.0.1-${shortRev}";
-                src = pkgs.lib.fileset.toSource {
-                  root = ./.;
-                  fileset = pkgs.lib.fileset.unions [
-                    ./src
-                    ./CMakeLists.txt
-                    ./cmake
-                    ./tests
-                  ];
-                };
-                nativeBuildInputs = deps.tools ++ [ pkgs.makeWrapper ];
-                buildInputs = deps.libs;
-                cmakeFlags = [
-                  (pkgs.lib.cmakeFeature "N64_GIT_COMMIT_HASH" rev) # Flakes do not have access to the .git dir, so we'll set this manually
-                  (pkgs.lib.cmakeFeature "CMAKE_BUILD_TYPE" "Release")
-                ];
-                passthru.exePath = "/bin/n64";
-                postInstall = ''
-                  wrapProgram $out/bin/n64 --set LD_LIBRARY_PATH ${pkgs.vulkan-loader}/lib
-                '';
-              };
-          });
-
-      apps = eachSystem
-        (system: {
-          default = {
-            type = "app";
-            program = "${self.packages.${system}.default}/bin/n64";
+      packages.default = pkgs.stdenv.mkDerivation
+        {
+          pname = "dgb-n64";
+          version = "0.0.1-${shortRev}";
+          src = pkgs.lib.fileset.toSource {
+            root = ./.;
+            fileset = pkgs.lib.fileset.unions [
+              ./src
+              ./CMakeLists.txt
+              ./cmake
+              ./tests
+            ];
           };
-        });
-    };
+          nativeBuildInputs = tools ++ [ pkgs.makeWrapper ];
+          buildInputs = libs;
+          cmakeFlags = [
+            (pkgs.lib.cmakeFeature "N64_GIT_COMMIT_HASH" rev) # Flakes do not have access to the .git dir, so we'll set this manually
+            (pkgs.lib.cmakeFeature "CMAKE_BUILD_TYPE" "Release")
+          ];
+          passthru.exePath = "/bin/n64";
+          postInstall = ''
+            wrapProgram $out/bin/n64 --set LD_LIBRARY_PATH ${pkgs.vulkan-loader}/lib
+          '';
+        };
+
+      apps.default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/n64";
+      };
+
+      devShells.default = pkgs.mkShell
+        {
+          buildInputs = tools ++ libs ++ devShellTools;
+          CPATH = "${lib_cpath}:${extra_cpath}";
+          LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+        };
+    }
+  );
 }
