d888276ea135ee99d64b9f41d763a060cb2fa5d2
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-03 03:28:19 -0700

    Make a bit more generic

--- flake.nix
+++ flake.nix
@@ -9,43 +9,49 @@
   outputs = { self, nixpkgs, systems }:
     let
       eachSystem = nixpkgs.lib.genAttrs (import systems);
+      get_deps = (pkgs:
+        let
+          llvmPackage = pkgs.llvmPackages_18;
+          libcxx = llvmPackage.libraries.libcxx;
+          clang = llvmPackage.libcxxClang;
+        in
+        {
+          clang = clang;
+          libcxx = libcxx;
+          tools = [
+            pkgs.cmake
+            clang
+            pkgs.ninja
+            pkgs.shaderc
+            pkgs.pkg-config
+            pkgs.gdb
+            pkgs.vulkan-loader
+            pkgs.mold-wrapped
+          ];
+
+          libs = [
+            pkgs.SDL2
+            pkgs.capstone
+            pkgs.dbus
+            pkgs.bzip2
+            libcxx
+          ];
+        });
     in
     {
       devShells = eachSystem
         (system:
           let
             pkgs = nixpkgs.legacyPackages.${system};
-            llvmPackage = pkgs.llvmPackages_18;
-
-            libcxx = llvmPackage.libraries.libcxx;
-            clang = llvmPackage.libcxxClang;
-
-            tools = [
-              pkgs.cmake
-              clang
-              pkgs.ninja
-              pkgs.shaderc
-              pkgs.pkg-config
-              pkgs.gdb
-              pkgs.vulkan-loader
-              pkgs.mold-wrapped
-            ];
-
-            libs = [
-              pkgs.SDL2
-              pkgs.capstone
-              pkgs.dbus
-              pkgs.bzip2
-              libcxx
-            ];
-            lib_cpath = pkgs.lib.makeSearchPathOutput "dev" "include" (libs);
+            deps = (get_deps pkgs);
+            lib_cpath = pkgs.lib.makeSearchPathOutput "dev" "include" (deps.libs);
             # Taken from CMake's ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES}
-            extra_cpath = "${libcxx.dev}/include/c++/v1:${clang}/resource-root/include:${pkgs.glibc.dev}/include";
+            extra_cpath = "${deps.libcxx.dev}/include/c++/v1:${deps.clang}/resource-root/include:${pkgs.glibc.dev}/include";
           in
           {
             default = pkgs.mkShell
               {
-                buildInputs = tools ++ libs;
+                buildInputs = deps.tools ++ deps.libs;
                 CPATH = "${lib_cpath}:${extra_cpath}";
                 LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
               };
