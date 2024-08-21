be5dbc9a1e403d430695b25d9f56e42757ce2dd4
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-21 00:36:37 -0700

    breakpoints in debugger

--- src/debugger/debugger.h
+++ src/debugger/debugger.h
@@ -9,5 +9,7 @@ void debugger_tick();
 void debugger_step();
 bool check_breakpoint(u64 address);
 void debugger_cleanup();
+void n64_debug_set_breakpoint(u64 address);
+void n64_debug_clear_breakpoint(u64 address);
 
 #endif //N64_DEBUGGER_H
--- src/frontend/http_api.cpp
+++ src/frontend/http_api.cpp
@@ -29,6 +29,8 @@ std::thread t;
     return; \
 } while(0)
 
+#define HTTP_OK do { res.set_content("OK", "application/json"); } while (0)
+
 template <typename T>
 T debugger_read(u32 address) {
     T result = 0;
@@ -49,6 +51,19 @@ T debugger_read(u32 address) {
     return result;
 }
 
+u64 parse_address(std::string s_address) {
+    u64 address = std::stoull(s_address, nullptr, 16);
+    // If it's a 32 bit address
+    if (address <= 0xFFFFFFFF) {
+        // And it wasn't explicitly specified to be not sign extended
+        if (!s_address.starts_with("00000000") && !s_address.starts_with("0x00000000")) {
+            // Sign extend it
+            address = (s64)((s32)address);
+        }
+    }
+    return address;
+}
+
 void http_api_init() {
     logalways("http_api_init listening on: %s:%d\n", n64_settings.http_api_host, n64_settings.http_api_port);
 
@@ -56,7 +71,7 @@ void http_api_init() {
         std::string s_size = req.path_params.at("size");
         std::string s_address = req.path_params.at("address");
 
-        u64 vaddr = std::stoull(s_address, nullptr, 16);
+        u64 vaddr = parse_address(s_address);
         u64 size = 0;
         try {
             size = std::stoull(s_size, nullptr, 0); // autodetect base
@@ -110,31 +125,68 @@ void http_api_init() {
     });
 
     svr.Get("/registers", [&](const httplib::Request& req, httplib::Response& res) {
-            json result;
-            result["pc"] = N64CPU.pc;
-            result["hi"] = N64CPU.mult_hi;
-            result["lo"] = N64CPU.mult_lo;
-            for (int i = 0; i < 32; i++) {
-                result[register_names[i]] = N64CPU.gpr[i];
+        json result;
+        result["pc"] = N64CPU.pc;
+        result["hi"] = N64CPU.mult_hi;
+        result["lo"] = N64CPU.mult_lo;
+        for (int i = 0; i < 32; i++) {
+            result[register_names[i]] = N64CPU.gpr[i];
+        }
+        res.set_content(result.dump(), "application/json");
+    });
+
+    auto get_breakpoints = [&](const httplib::Request& req, httplib::Response& res) {
+        json result = json::array({});
+        for (const auto& [ address, breakpoint ] : breakpoints) {
+            if (!breakpoint.temporary) {
+                result.push_back(std::format("{:016X}", address));
             }
-            res.set_content(result.dump(), "application/json");
+        }
+        res.set_content(result.dump(), "application/json");
+    };
+
+    svr.Get("/breakpoints", get_breakpoints);
+
+    svr.Get("/breakpoints/:operation/:address", [&](const httplib::Request& req, httplib::Response& res) {
+        std::string operation = req.path_params.at("operation");
+        u64 address = parse_address(req.path_params.at("address"));
+
+        if (operation == "set") {
+            logalways("%s", std::format("Set breakpoint at {:016X}", address).c_str());
+            n64_debug_set_breakpoint(address);
+            get_breakpoints(req, res);
+        } else if (operation == "clear") {
+            n64_debug_clear_breakpoint(address);
+            logalways("%s", std::format("Cleared breakpoint at {:016X}", address).c_str());
+            get_breakpoints(req, res);
+        } else if (operation == "mute") {
+            HTTP_ERROR(std::string("Unimplemented operation: ") + operation, BadRequest_400);
+        } else if (operation == "unmute") {
+            HTTP_ERROR(std::string("Unimplemented operation: ") + operation, BadRequest_400);
+        } else {
+            HTTP_ERROR(std::string("Invalid operation: ") + operation, BadRequest_400);
+        }
     });
 
     svr.Get("/control/break", [&](const httplib::Request& req, httplib::Response& res) {
         n64sys.debugger_state.broken = true;
+        HTTP_OK;
     });
 
     svr.Get("/control/step", [&](const httplib::Request& req, httplib::Response& res) {
         debugger_step();
+        HTTP_OK;
     });
 
     svr.Get("/control/continue", [&](const httplib::Request& req, httplib::Response& res) {
         n64sys.debugger_state.broken = false;
+        HTTP_OK;
     });
 
     svr.Get("/control/quit", [&](const httplib::Request& req, httplib::Response& res) {
         n64sys.debugger_state.broken = false; // if we're paused, we need to unpause for the quit to work
         n64_request_quit();
+        HTTP_OK;
     });
 
     t = std::thread(http_server_thread);
