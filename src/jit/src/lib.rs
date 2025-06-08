a2245499e3d08c7374afb736bf6caf0abd78f8a5
Dillon Beliveau <dillon@dgb.sh>
2025-06-07 17:38:35 -0700

    Remove IR from here (we're going to use dgbir)

--- src/jit/src/lib.rs
+++ src/jit/src/lib.rs
@@ -1,6 +1,4 @@
 mod mips_parser;
-mod ir;
-mod ir_to_x64;
 
 #[no_mangle]
 pub unsafe extern fn rs_jit_compile_new_block(instructions: *mut u32, num_instructions: usize, virtual_address: u64, physical_address: u32) {
