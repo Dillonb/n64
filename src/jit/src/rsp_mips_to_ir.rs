5f33ab05bf552db62bd1bb3cca9ad13dba37ff61
Dillon Beliveau <dillon@dgb.sh>
2026-02-24 02:04:09 -0800

    Quiet dead code warnings for now

--- src/jit/src/rsp_mips_to_ir.rs
+++ src/jit/src/rsp_mips_to_ir.rs
@@ -18,23 +18,23 @@ use crate::{
 };
 
 pub struct RspMipsToIrContext {
-    read_byte: usize,
-    read_half: usize,
+    read_byte_: usize,
+    read_half_: usize,
     read_word: usize,
-    write_byte: usize,
-    write_half: usize,
-    write_word: usize,
+    write_byte_: usize,
+    write_half_: usize,
+    write_word_: usize,
 }
 
 impl RspMipsToIrContext {
     pub fn default() -> Self {
         Self {
-            read_byte: n64_rsp_read_byte_noinline as usize,
-            read_half: n64_rsp_read_half_noinline as usize,
+            read_byte_: n64_rsp_read_byte_noinline as usize,
+            read_half_: n64_rsp_read_half_noinline as usize,
             read_word: n64_rsp_read_word_noinline as usize,
-            write_byte: n64_rsp_write_byte_noinline as usize,
-            write_half: n64_rsp_write_half_noinline as usize,
-            write_word: n64_rsp_write_word_noinline as usize,
+            write_byte_: n64_rsp_write_byte_noinline as usize,
+            write_half_: n64_rsp_write_half_noinline as usize,
+            write_word_: n64_rsp_write_word_noinline as usize,
         }
     }
 }
