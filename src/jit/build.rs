03a19185c0993dd7007353ea2577487ae94b9aae
Dillon Beliveau <dillon@dgb.sh>
2026-03-10 02:25:11 -0700

    fix some bindgen issues

--- src/jit/build.rs
+++ src/jit/build.rs
@@ -23,10 +23,17 @@ fn main() {
         .blocklist_function("qfcvt")
         .blocklist_function("qfcvt_r")
         .blocklist_function("qgcvt")
-        .blocklist_function("strtold");
+        .blocklist_function("strtold")
+        // NEON multi-vector struct types - these fail alignment checks because the type
+        // aliases align to the _element_ in Rust, when they align to the whole thing in C.
+        // I don't use these types in Rust and shouldn't ever need to, so just blocklist all of them.
+        .blocklist_type(".*x\\d+x[234]_t");
 
     #[cfg(target_os = "windows")]
     let bindings = bindings.clang_arg("-DN64_WIN");
+
+    #[cfg(target_arch = "aarch64")]
+    let bindings = bindings.clang_arg("-DN64_USE_NEON");
 
     let bindings = bindings.generate().expect("Unable to generate bindings");
 
